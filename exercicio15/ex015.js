e2596267fe880e81b3cc9f903d8572831c5202e4
HenrickyDev <henrique.devgaldino@outlook.com>
2022-03-23 05:06:52 -0300

    a

--- exercicio15/ex015.js
+++ exercicio15/ex015.js
@@ -6,3 +6,11 @@ Depois mostre os números em ordem crescente
 
 Entregar o fluxograma e o código .js
 */
+
+console.log('Progrma que solicite 2 números, para depois mostras em ondem crescente...');
+prompt = require('prompt-sync') ();
+let res = [ 
+    a = prompt('Número 1: '),
+    b = prompt('Número 2: '),
+];
+console.log(`${res.sort()}`);
