1782b197637a48d2b4d7ca488db5185843fc9f75
HenrickyDev <henrique.devgaldino@outlook.com>
2022-03-23 01:10:29 -0300

    atualizar

--- aula7teste/ex010exemplo.js
+++
@@ -1,5 +0,0 @@
-console.log('Programa para informar o preço da gasolina atualmente...')
-
-const readline = require('readline-sync')
-
-let n1 = readline.question('Informe o preço atual da gasolina ==> ')
---
+++ exercicio11/ex011.js
@@ -0,0 +1,18 @@
+/*
+ Faça um programa que solicite a temperatura atual para o usuário em graus Celsius.
+ E este programa deverá mostrar apenas uma das seguintes frases:
+
+ Está calor quando a temperatura for maior que 25
+ Está frio quando a temperatura não for maior que 25 
+*/
+
+console.log('Programa para ler a temperatura atual digitada pelo usuário...');
+prompt = require('prompt-sync') ();
+console.log('Digite a temperatura atual:');
+temperatura = prompt('' , + '°');
+
+if (temperatura >= 25) {;
+    console.log(`${temperatura + '°'} Está calor!`);
+} else {;
+    console.log(`${temperatura + '°'} Está frio!`);  
+};
