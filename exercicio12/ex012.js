36fedb4efbeb35bfadc65ffdb4630824380b3128
HenrickyDev <henrique.devgaldino@outlook.com>
2022-03-23 01:53:01 -0300

    atualização

---
+++ exercicio12/ex012.js
@@ -0,0 +1,24 @@
+/*
+Faça um programa que solicite ao usuário para informar o valor da humidade atual do ambiente, correspondente a porcentagem de humidade no ar. 
+E este programa deverá mostrar apenas uma das seguintes frases:
+
+O ar está húmido quando a humidade for maior que 70
+O ar está seco quando a humidade for maior igual a 30 e menor igual a 70
+O ar está muito seco quando a humidade for menor que 30
+
+Utilize o if encadeado para resolver esta questão, veja o fluxograma em anexo
+*/
+
+console.log('Programa que informa o valor atual da humidade do ambiente')
+
+prompt = require('prompt-sync') ()
+
+let humidade = prompt('Informe a humidade atual: ')
+
+if (humidade > 70) {
+    console.log('O ar está húmido!')
+} if (humidade >=30 && humidade <=70) {
+    console.log('O ar está seco!')
+} else if (humidade < 30) {
+    console.log('O ar está muito seco!')
+}
