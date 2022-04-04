acd4f41ae59dcac0de00492cb03ba179d63e06e7
HenrickyDev <102200241+Henrique031@users.noreply.github.com>
2022-04-04 08:21:22 -0300

    Update ex016-1.js

--- exercicio16/ex016-1.js
+++ exercicio16/ex016-1.js
@@ -19,5 +19,5 @@ if ((n1 < n2 && n1 < n3) && (n2 > n1 && n2 < n3) && (n3 > n1 && n3 > n2) ) {
 } if ((n1 > n2 && n1 < n3) && (n2 < n1 && n2 < n3) && (n3 > n1 && n3 > n2) ) {
     console.log(n2, n1, n3)
 } if ((n1 > n2 && n1 > n3) && (n2 < n1 && n2 > n3) && (n3 < n1 && n3 < n2) ) {
-    console.log(n3, n2, n1)
+   console.log(n3, n2, n1)
 };
