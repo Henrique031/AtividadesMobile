edc8ab5c9968ea20d9a8c119011aad3f4eadb912
Henrique Galdino <102200241+Henrique031@users.noreply.github.com>
2022-05-23 13:45:15 -0300

    .

--- exercicio41/App.js
+++ exercicio41/App.js
@@ -28,30 +28,25 @@ export default function App() {
 import React from 'react';
 import { Text, View, TextInput, Button, ImageBackground } from 'react-native';
 import imgPets from './assets/Pets.png'
-import Tela2 from './src/views/Tela2'
 
 
-const Separador = () => {
-  return(
-    <View style={{borderColor: 'black', borderBottomWidth: 1}} />
-    )
-  }
-  export default (props) => {
 
-  const Botao = () => {
-    return(
-      <View style={{flexGrow: 3, width: 150, marginStart: 130, marginTop: 20, alignItems: 'center'}}>
-                <Button title='Cadastrar' style={{}} onPress={() => cadastrar()} />
-            </View>
-        )
-      }
+  export default function App() {
 
-      const cadastrar = () => {
-        console.warn(nomePet)
-        console.warn(raca)
-        console.warn(peso)
-        console.warn(nasc)
-      }
+    const Botao = () => {
+      return(
+        <View style={{flexGrow: 3, width: 150, marginStart: 130, marginTop: 20, alignItems: 'center'}}>
+                  <Button title="Cadastrar" onPress={() => cadastrar()} />
+              </View>
+          )
+        }
+
+    const cadastrar = () => {
+      console.warn(nomePet)
+      console.warn(raca)
+      console.warn(peso)
+      console.warn(nasc)
+    }
 
       const [ nomePet, setNome] = React.useState(null)
       const [ raca, setRaca] = React.useState(null)
@@ -60,40 +55,47 @@ const Separador = () => {
       
   return (
     <View style={{flex: 1}}>
+
+      <ImgFundo />
       
-      <ImageBackground source={imgPets} style={{flex: 10}} >
+      <View style={{flexGrow: 13, justifyContent: 'space-around',}}>
+        <Formulario nome="Digite o nome do seu pet" sets={setNome} variavel={nomePet} />
+        <Formulario nome="Digite a raçã do seu pet" sets={setRaca} variavel={raca} />      
+        <Formulario nome="Digite o peso do seu pet" sets={setPeso} variavel={peso} />  
+        <Formulario nome="Digite o Nascimento do seu pet" sets={setNasc} variavel={nasc} />
+      </View>   
+
+      <Botao titulo="Cadastrar" />
+    
+    </View> 
+  )
+}
+
+const ImgFundo = () => {
+  return(
+    <ImageBackground source={imgPets} style={{flex: 10}} >
         <View style={{alignItems: 'center', }} >
         <Text style={{color: 'orange', fontSize: 40, backgroundColor: 'rgba(155,155,155, 0.3)', paddingHorizontal: 157}} >PETS</Text>
         </View>
       </ImageBackground>
 
-      <View style={{flexGrow: 13, justifyContent: 'space-around',}}>
-        <View style={{marginBottom: 8}}>
-          <TextInput placeholder='Nome do Pet' style={{paddingStart: 7}} onChangeText={setNome} value={props.nomePet} />
-
-        <Separador />
-
-        </View>
-        <View style={{marginBottom: 8}}>
-          <TextInput placeholder='Raça' style={{paddingStart: 7}} onChangeText={setRaca} value={props.raca} />
-
-        <Separador />
-
-        </View>
-        <View style={{marginBottom: 8}}>
-          <TextInput placeholder='Peso' style={{paddingStart: 7}} onChangeText={setPeso} value={props.peso} />
-
-        <Separador />
-
-        </View>
-        <View style={{marginBottom: 8}}>
-        <TextInput placeholder='Nacimento' style={{paddingStart: 7}} onChangeText={setNasc} value={props.nasc} keyboardType='numeric' />
-        <Separador />
-        </View>
-      </View>  
-
-      <Botao />
-    
-    </View> 
   )
 }
+
+const Formulario = (props) => {
+
+  const Separador = () => {
+  return(
+    <View style={{borderColor: 'black', borderBottomWidth: 1}} />
+    )
+  }
+
+  return(
+
+      <View style={{marginBottom: 8}}>
+        <TextInput placeholder={props.nome} style={{paddingStart: 7}} onChangeText={props.sets} value={props.variavel} />
+        <Separador />
+      </View>
+    )
+}
+
